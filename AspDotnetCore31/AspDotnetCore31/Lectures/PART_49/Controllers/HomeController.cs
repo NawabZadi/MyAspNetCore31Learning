5e3d788485f8097cce41d4cf9a2440a8fb5caf57
Nawab Zadi <nawabzadi0819@gmail.com>
2024-04-30 12:21:29 +0500

    PART 49

---
+++ AspDotnetCore31/AspDotnetCore31/Lectures/PART_49/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
+using PART_19.Models;
+using System.Diagnostics;
+
+namespace PART_19.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public ViewResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                Employee newEmployee = _employeeRepository.Add(employee);
+                return RedirectToAction("details", new { id = newEmployee.Id });
+            }
+
+            return View();
+        }
+
+    }
+    
+}
---
+++ AspDotnetCore31/AspDotnetCore31/Lectures/PART_49/Models/IEmployeeRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace PART_19.Models
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployee(int Id);
+        IEnumerable<Employee> GetAllEmployee();
+        Employee Add(Employee employee);
+        Employee Update(Employee employeeChanges);
+        Employee Delete(int Id);
+    }
+}
