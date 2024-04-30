021f486c448a6351cdab9bd86a280409fd12c226
Nawab Zadi <nawabzadi0819@gmail.com>
2024-04-30 11:58:20 +0500

    PART 44

---
+++ AspDotnetCore31/AspDotnetCore31/Lectures/PART_44/IEmployeeRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace PART_19.Models
+{
+    public interface IEmployeeRepository
+    {
+        Employee GetEmployee(int Id);
+        IEnumerable<Employee> GetAllEmployees();
+        Employee Add(Employee employee);
+    }
+}
---
+++ AspDotnetCore31/AspDotnetCore31/Lectures/PART_44/Models/MockEmployeRepository.cs
@@ -0,0 +1,35 @@
+﻿
+
+namespace PART_19.Models
+{
+    public class MockEmployeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Amina", Department = "CS", Email = "Amina@gmail.com" } 
+            };
+        }
+
+        public Employee Add(Employee employee)
+        {
+            employee.Id = _employeeList.Max(e => e.Id) + 1;
+            _employeeList.Add(employee);
+            return employee;
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+        
+}
